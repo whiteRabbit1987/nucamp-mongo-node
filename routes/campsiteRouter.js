7305b481aa4d74d2040266c26f84eb9b0f4ea7e6
David <89405283+whiteRabbit1987@users.noreply.github.com>
2024-11-23 07:50:38 -0600

    Mongoose Population

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -6,6 +6,7 @@ const authenticate = require('../authenticate');
 campsiteRouter.route('/')
 .get((req, res, next) => {
 Campsite.find()
+.populate('comments.author')
 .then(campsites => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
@@ -40,6 +41,7 @@ Campsite.deleteMany()
 campsiteRouter.route('/:campsiteId')
 .get((req, res, next) => {
 Campsite.findById(req.params.campsiteId)
+.populate('comments.author')
 .then(campsite => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
@@ -75,6 +77,7 @@ Campsite.findByIdAndDelete(req.params.campsiteId)
 campsiteRouter.route('/:campsiteId/comments')
 .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         if (campsite) {
             res.statusCode = 200;
@@ -92,6 +95,7 @@ campsiteRouter.route('/:campsiteId/comments')
     Campsite.findById(req.params.campsiteId)
     .then(campsite => {
         if (campsite) {
+            req.body.author = req.user._id;
             campsite.comments.push(req.body);
             campsite.save()
             .then(campsite => {
@@ -138,6 +142,7 @@ campsiteRouter.route('/:campsiteId/comments')
 campsiteRouter.route('/:campsiteId/comments/:commentId')
 .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
+    .populate('comments.author')
     .then(campsite => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
             res.statusCode = 200;
--- routes/users.js
+++ routes/users.js
@@ -10,23 +10,30 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/signup', (req, res) => {
-    User.register(
-        new User({ username: req.body.username }),
-        req.body.password,
-        err => {
-            if (err) {
-                res.statusCode = 500;
-                res.setHeader('Content-Type', 'application/json');
-                res.json({err: err});
-            } else {
-                passport.authenticate('local')(req, res, () => {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'application/json');
-                    res.json({success: true, status: 'Registration Successful!'});
-                });
+    const user = new User({ username: req.body.username });
+
+    User.register(user, req.body.password)
+        .then(registeredUser => {
+            if(req.body.firstname){
+                registeredUser.firstname = req.body.firstname;
             }
-        }
-    );
+            if(req.body.lastname){
+                registeredUser.lastname = req.body.lastname;
+            }
+            return registeredUser.save();
+        })
+        .then(() => {
+            passport.authenticate('local')(req, res, () => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({ success: true, status: 'Registration Successful!'});
+            });
+        })
+        .catch(err => {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ err: err });
+        });
 });
 
 router.post('/login', passport.authenticate('local', { session: false }), (req, res) => {
